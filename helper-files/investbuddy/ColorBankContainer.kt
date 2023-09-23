bc01bdab0d27a85f09f5b1326885a3861caa76f0
Kauushal <kaushal.devil009@gmail.com>
2023-09-24 02:33:25 +0530

    Added helper files for investbuddy flavor

---
+++ helper-files/investbuddy/ColorBankContainer.kt
@@ -0,0 +1,35 @@
+package com.droidcon.wealthbuddy.ui.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+object ColorBankContainer : ColorBank {
+    override val primary = Color(0xFFB91D1D)
+    override val onPrimary = Color(0xFFFFFFFF)
+    override val primaryContainer = Color(0xFFC4C4C4)
+    override val onPrimaryContainer = Color(0xFF410002)
+    override val secondary = Color(0xFF775652)
+    override val onSecondary = Color(0xFFFFFFFF)
+    override val secondaryContainer = Color(0xFFFFDAD6)
+    override val onSecondaryContainer = Color(0xFF2C1513)
+    override val tertiary = Color(0xFF715B2E)
+    override val onTertiary = Color(0xFFFFFFFF)
+    override val tertiaryContainer = Color(0xFFFDDFA6)
+    override val background = Color(0xFFFFCDD2)
+    override val onBackground = Color(0xFFD50000)
+
+    override val primaryDark = Color(0xFFFFB4AB)
+    override val onPrimaryDark = Color(0xFF690005)
+    override val primaryContainerDark = Color(0xFF121212)
+    override val onPrimaryContainerDark = Color(0xFFFFDAD6)
+    override val secondaryDark = Color(0xFFE7BDB8)
+    override val onSecondaryDark = Color(0xFF442926)
+    override val secondaryContainerDark = Color(0xFF5D3F3C)
+    override val onSecondaryContainerDark = Color(0xFFFFDAD6)
+    override val tertiaryDark = Color(0xFFE0C38C)
+    override val onTertiaryDark = Color(0xFF3F2E04)
+    override val tertiaryContainerDark = Color(0xFF584419)
+    override val backgroundDark = Color(0xFF8B0000)
+    override val onBackgroundDark = Color(0xFFE2E3DD)
+}
